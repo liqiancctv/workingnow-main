0dace6c0ba2c2104dcbf4e41eb0adfb22bf3abcf
WorkingNow <g6kmyt7rz5@privaterelay.appleid.com>
2026-06-10 13:57:20 +0800

    modi settings

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { fallbackLng, languages } from './app/i18n/settings'
+import { fallbackLng, languages } from './i18n/settings'
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
